afd702b73f954855955ee0affeb88bca2d3a805b
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-03-20 20:50:20 +0100

    feat(movies) work in progress

--- react-vanilla/movies/src/components/movie-list/MovieList.js
+++ react-vanilla/movies/src/components/movie-list/MovieList.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import MovieElement from './MovieElement';
 
 export default class MovieList extends Component {
 
     render() {
-        return <div>Movie List</div>
+        return (
+            <div className="w-75 d-flex flex-row flex-wrap align-content-start">
+                <MovieElement />
+                <MovieElement />
+                <MovieElement />
+                <MovieElement />
+                <MovieElement />
+                <MovieElement />
+            </div>
+        );
     }
 
 }
