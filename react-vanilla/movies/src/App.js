d15679ef8852759ab265ffda70272ea0b499f716
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-03-20 20:50:20 +0100

    feat(movies) work in progress

--- react-vanilla/movies/src/App.js
+++ react-vanilla/movies/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Header, MovieList, MovieDetails } from './components';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App d-flex flex-column">
+                <Header />
+                <div className="d-flex flex-row flex-fill pt-4 p-2" >
+                    <MovieList />
+                    <MovieDetails />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- react-vanilla/movies/src/components/movie-details/MovieDetails.js
+++ react-vanilla/movies/src/components/movie-details/MovieDetails.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class MovieDetails extends Component {
 
     render() {
-        return <div>Movie Details</div>
+        return <div className="w-25">Movie Details</div>
     }
 
 }
