8f928fd77badc7fc6a7e9c761772c52ab35f5102
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-03-27 00:03:54 +0100

    feat(movies) work in progress

--- react-vanilla/movies/src/components/movie-list/MovieElement.js
+++ react-vanilla/movies/src/components/movie-list/MovieElement.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
 export default class MovieElement extends Component {
 
+    mouseEnter = () => {
+        this.props.updateSelectedMovie(this.props.movie.title);
+    }
+
     render() {
         return (
-            <div className="w-25 p-2">
-                <div className="card">
-                    <img alt="film" src="https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">GLADIATOR</h5>
-                        <p className="card-text">Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire avec une bravoure et un dévouement exemplaires.</p>
+            <div onMouseEnter={ this.mouseEnter } className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" alt="film" src={ this.props.movie.img } />
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5> { this.props.movie.title } </h5>
+                        <hr className="w-100" />
+                        <span>{ this.props.movie.details }</span>
                     </div>
                 </div>
             </div>
