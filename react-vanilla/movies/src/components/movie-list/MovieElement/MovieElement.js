1f0a545ebd5a0ff0b596aebb63568b260f69a25b
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-01 17:00:03 +0200

    feat(movies) work in progress

--- react-vanilla/movies/src/components/movie-list/MovieElement/MovieElement.js
+++ react-vanilla/movies/src/components/movie-list/MovieElement/MovieElement.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Style from './MovieElement.module.scss';
 
 export default class MovieElement extends Component {
 
@@ -8,7 +9,7 @@ export default class MovieElement extends Component {
 
     render() {
         return (
-            <div onMouseEnter={ this.mouseEnter } className="w-50 p-2">
+            <div onMouseEnter={ this.mouseEnter } className={ "d-flex flex-row bg-light " + Style.container }>
                 <div className="border d-flex">
                     <img width="150" height="200" alt="film" src={ this.props.movie.img } />
                     <div className="flex-fill d-flex flex-column p-3">
