f35a101ea95b12c1334137b557f9b7c7b4a2f829
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-08 23:16:23 +0200

    feat(movies) add search

--- react-vanilla/movies/src/components/index.js
+++ react-vanilla/movies/src/components/index.js
@@ -3,3 +3,5 @@ export {default as MovieDetails} from './movie-details/MovieDetails'
 export {default as MovieElement} from './movie-list/MovieElement/MovieElement'
 export {default as MovieList} from './movie-list/MovieList'
 export { default as Loading } from './utils/Loading';
+export { default as SearchBar } from './search-bar/SearchBar'
+
