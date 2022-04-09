e5f03c320c43e856252ab6f68f5efd1ee4670671
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-09 11:30:35 +0200

    feat(movies)on click

--- react-vanilla/movies/src/components/movie-list/MovieElement/MovieElement.js
+++ react-vanilla/movies/src/components/movie-list/MovieElement/MovieElement.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Style from './MovieElement.module.scss';
 
 export default class MovieElement extends Component {
 
-
-    mouseEnter = () => {
+    click = () => {
         this.props.updateSelectedMovie(this.props.movie.title);
     }
 
-
     render() {
         return (
-            <div onMouseEnter={ this.mouseEnter } className={ "d-flex flex-row bg-light " + Style.container }>
+            <div onClick={this.click} className={"d-flex flex-row bg-light " + Style.container}>
                 <div className="border d-flex">
-                    <img alt='film' width='185' src={this.props.movie.img} />
+                    <img alt='film' width='185' src={this.props.movie.img}/>
                     <div className="flex-fill d-flex flex-column p-3">
-                        <h5> { this.props.movie.title } </h5>
-                        <hr className="w-100" />
-                        <span>{ this.props.movie.details }</span>
+                        <h5> {this.props.movie.title} </h5>
+                        <hr className="w-100"/>
+                        <span>{this.props.movie.details}</span>
                     </div>
                 </div>
             </div>
