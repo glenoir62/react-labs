107189b6b291357f0e4a5f965d01380ae98dffa7
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-09 12:03:46 +0200

    feat(movies) refacto

--- react-vanilla/movies/src/features/films/components/movie-list/MovieList.js
+++ react-vanilla/movies/src/features/films/components/movie-list/MovieList.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import MovieElement from './MovieElement/MovieElement';
+import {MovieDetails} from "../index";
 
 export default class MovieList extends Component {
 
     render() {
         return (
-            <div className='w-75 d-flex flex-row flex-wrap justify-content-center' >
-            {this.props.movies.map((m, index) => (
-                    <MovieElement key={m.title + index}
-                                  movie={m}
-                                  updateSelectedMovie={() => { this.props.updateSelectedMovie(index) }} />
-                ))}
+            <div className='w-75 d-flex flex-row flex-wrap justify-content-center'>
+                {this.props.movies && this.props.movies.length > 0 ?
+                    (
+                        this.props.movies.map((m, index) => (
+                            <MovieElement key={m.title + index}
+                                          movie={m}
+                                          updateSelectedMovie={() => {
+                                              this.props.updateSelectedMovie(index)
+                                          }}/>
+                        ))
+                    ) : (<div className="alert alert-warning" role="alert">
+                        No movies
+                    </div>)
+                }
             </div>
         );
     }
