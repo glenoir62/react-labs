2afe6f398f730b813b10c03d656c001d0614639a
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-18 17:00:11 +0200

    feat(favori) add favori

--- react-vanilla/movies/src/components/header/Header.js
+++ react-vanilla/movies/src/components/header/Header.js
@@ -8,10 +8,7 @@ export default class Header extends Component {
         return (
             <header className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">AlloMovie</a>
-                <button className="navbar-toggler">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse">
+                <div className="navbar navbar-expand">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <NavLink to="/films" className="nav-link"> Home </NavLink>
--- react-vanilla/movies/src/features/favoris/index.js
+++ react-vanilla/movies/src/features/favoris/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { FavoriList } from './components';
+import Loading from '../../components/utils/Loading';
 
 export default (props) => {
-    return <h1>Favoris</h1>
+    return (
+        <div className="d-flex flex-row flex-fill pt-4 p-2" >
+
+            { props.loaded ? (
+                <div className="d-flex flex-row flex-fill pt-4 p-2" >
+                    <FavoriList
+                        favoris={ props.favoris }
+                        removeFavori={ props.removeFavori }
+                    />
+                </div>
+            ) : (
+                <Loading />
+            ) }
+        </div>
+    )
 }
