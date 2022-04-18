59a377561fadcd41a8960219315fcd6d382352a4
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-18 18:20:40 +0200

    feat(redux) init redux

--- react-vanilla/react-redux/src/components/TodoItem.js
+++ react-vanilla/react-redux/src/components/TodoItem.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const TodoItem = (props) => {
-  return (
-    <li className="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action" >
-      <span> x </span>
-      <span>
+export default ({todo}) => {
+    return (
+        <li className="list-group-item d-flex flex-row justify-content-between align-items-center list-group-item-action" >
+            <span> {todo.name} </span>
+            <span>
         <input className="mx-3"  type="checkbox" />
         <button className="btn btn-sm btn-danger">delete</button>
       </span>
-    </li>
-  )
+        </li>
+    )
 }
-
-export default TodoItem;
--- react-vanilla/react-redux/src/components/TodoList.js
+++ react-vanilla/react-redux/src/components/TodoList.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import { connect } from 'react-redux';
+import { VisibilityFilters } from "../store/actions";
 
-const TodoList = (props) => {
-  return (
-    <ul className="list-group">
-      <TodoItem />
-    </ul>
-  )
+const TodoList = ({todos}) => {
+    return (
+        <ul className="list-group">
+            { todos && todos.map(t =>
+                <TodoItem key={t.name} todo={t} />
+            )}
+        </ul>
+    )
 }
 
-export default TodoList;
+export default connect(state => {
+    const filter = state.filter;
+    let todos;
+    switch(filter) {
+        case VisibilityFilters.SHOW_DONE: {
+            todos = state.todos.filter( t => t.done )
+            break;
+        }
+        case VisibilityFilters.SHOW_ACTIVE: {
+            todos = state.todos.filter( t => !t.done )
+            break;
+        }
+        default: {
+            todos = state.todos
+            break;
+        }
+    }
+    return {todos};
+})(TodoList);
