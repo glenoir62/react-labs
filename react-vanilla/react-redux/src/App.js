798a2bd087be86c0bc5aa1895311f7eaa1d2ae25
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-18 18:52:01 +0200

    feat(redux) init redux

--- react-vanilla/react-redux/src/App.js
+++ react-vanilla/react-redux/src/App.js
@@ -4,25 +4,25 @@ import Filter from './components/Filter';
 import TodoList from './components/TodoList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container p-5">
-        <h4>Ajouter une todo</h4>
-        <hr className="my-4" />
-        <AddTodo />
-        <hr className="my-4" />
-        <div className="card">
-          <div className="card-header d-flex flex-row align-items-center">
-            <span className="flex-fill">Todo list</span>
-            <Filter />
-          </div>
-          <div className="card-body">
-            <TodoList />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container p-5">
+                <h4>Ajouter une todo</h4>
+                <hr className="my-4" />
+                <AddTodo />
+                <hr className="my-4" />
+                <div className="card">
+                    <div className="card-header d-flex flex-row align-items-center">
+                        <span className="flex-fill">Todo list</span>
+                        <Filter />
+                    </div>
+                    <div className="card-body">
+                        <TodoList/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
