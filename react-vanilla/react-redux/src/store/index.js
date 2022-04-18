eebb892eb60de4bae91ca2c33b4aff01ee3eee7b
Guillaume Lenoir <guillaume.lenoir59@gmail.com>
2022-04-18 18:20:40 +0200

    feat(redux) init redux

---
+++ react-vanilla/react-redux/src/store/index.js
@@ -0,0 +1,8 @@
+import * as reducers from './reducers';
+import { combineReducers } from 'redux';
+import { createStore } from 'redux';
+
+const todoAppReducer = combineReducers(reducers);
+const store = createStore(todoAppReducer);
+
+export default store;
